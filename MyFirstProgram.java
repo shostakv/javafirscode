3a2df8a7d6dce242010cf2dafd597d487726867c
shostakv <shostakv@users.noreply.github.com>
2018-03-29 15:34:08 +0300

    Создан пакет для первой програми

--- MyFirstProgram.java
+++ MyFirstProgram.java
@@ -1,6 +1,7 @@
-public class MyFirstProgram {
+public class MyFirstProgram
+{
 
  public static void main(String[] args) {
-System.out.println("Hello worl");
+System.out.println("Hello world");
 }
 }
