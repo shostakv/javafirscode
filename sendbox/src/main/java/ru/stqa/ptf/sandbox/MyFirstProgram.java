6e21eefe5415d267495469811c468a0dc5a671d3
shostakv <shostakv@users.noreply.github.com>
2018-03-29 15:54:27 +0300

    Операцыъ множення дылення ы тд

--- sendbox/src/main/java/ru/stqa/ptf/sandbox/MyFirstProgram.java
+++ sendbox/src/main/java/ru/stqa/ptf/sandbox/MyFirstProgram.java
@@ -6,5 +6,22 @@ public class MyFirstProgram {
 
 
         System.out.println("Hello world");
+
+        System.out.println(2 + 2);
+        System.out.println(2 * 2);
+        System.out.println(2 / 2);
+        System.out.println(1 / 2);
+        System.out.println( 1.0 / 2.0);
+        System.out.println(2.0 / 2);
+
+        System.out.println("2" + "2");
+        System.out.println(2 + "22");
+
+
+        System.out.println(2 + 2 * 2);
+        System.out.println("2 + 2 =" + 2 + 2);
+        System.out.println("2 + 2 = " + (2 + 2));
+
+
     }
 }
