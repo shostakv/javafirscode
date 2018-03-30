42ca8bfebaa61cce6d7369ce8538dd153fdf40db
shostakv <shostakv@users.noreply.github.com>
2018-03-30 11:17:20 +0300

    Добавлено несколько фуекций

--- sendbox/src/main/java/ru/stqa/ptf/sandbox/MyFirstProgram.java
+++ sendbox/src/main/java/ru/stqa/ptf/sandbox/MyFirstProgram.java
@@ -2,9 +2,34 @@ package ru.stqa.ptf.sandbox;
 
 public class MyFirstProgram {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+    hello("Aleks");
+    hello("WORLD");
+
+    double len = 5;
+    System.out.println("Площадь квадрата со стороной " + len + " равна " + area(len));
+
+    double a = 11;
+    double b = 2;
+    System.out.println("Площа прямокутника зі стороною " + a + " і " + b + " равна "  + area(a, b) );
+
+  }
+
+  public static void hello(String somebody) {
+
+    System.out.println("Hello " + somebody);
+
+  }
+
+  public static double area(double l) {
+    return l * l;
+
+  }
+
+  public  static  double area(double a, double b) {
+    return  a * b;
+  }
 
 
-        System.out.println("Hello world");
-    }
+
 }
