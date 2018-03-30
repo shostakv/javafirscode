8e353b7037eb258158f9c4ef0791203369ca882d
shostakv <shostakv@users.noreply.github.com>
2018-03-30 10:33:02 +0300

    1

--- sendbox/src/main/java/ru/stqa/ptf/sandbox/MyFirstProgram.java
+++ sendbox/src/main/java/ru/stqa/ptf/sandbox/MyFirstProgram.java
@@ -4,34 +4,7 @@ public class MyFirstProgram {
 
     public static void main(String[] args) {
 
-         hello("world");
-         hello("Aleks");
-         hello("Car");
 
-         double len = 5;
-        System.out.println("Площадь квадрата равна со стороной " + len + " = " + area(len));
-
-        double a = 4;
-        double b = 6;
-        System.out.println("Площадь прямоугольника со стороной " + a + " и " + b + " равна " + area(a,b));
-
-
-
-
+        System.out.println("Hello world");
     }
-
-    public  static  void  hello(String somebody) {
-
-        System.out.println("Hello," + somebody +  "!" );
-    }
-    public  static  double area(double l) {
-        return l * l;
-    }
-
-    public  static  double  area(double a, double b) {
-        return  a * b;
-    }
-
-
-
-    }
+}
